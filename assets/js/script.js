3b78e07302ff6b9b0ca348420fa3675851b7e941
HampusMatt <hampusm.mattson@gmail.com>
2023-01-09 21:59:17 +0000

    Add imgs of animals to questions array

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const questions = [
     {
         id: 1,
         questionText: 'The sloth is so slow moving that algae can grow in their fur',
-        imgSrc: 'placeholder.png',
+        imgSrc: 'assets/images/sloth.jpg',
         options: [
           'True',
           'False',
@@ -12,7 +12,7 @@ const questions = [
       {
         id: 2,
         questionText: 'The horn of a rhino is actually made of hair',
-        imgSrc: 'placeholder.png',
+        imgSrc: 'assets/images/rhino.jpg',
         options: [
           'True',
           'False',
@@ -22,7 +22,7 @@ const questions = [
       {
         id: 3,
         questionText: 'The beluga whale is often reffered to as the "canaries of the sea" due to their distinctive vocalizations',
-        imgSrc: 'placeholder.png',
+        imgSrc: 'assets/images/beluga.jpg',
         options: [
           'Ture',
           'False',
@@ -32,7 +32,7 @@ const questions = [
       {
         id: 4,
         questionText: 'The American bald eagle is an iconic bird, but has since 1970 been considered an endangered species',
-        imgSrc: 'placeholder.png',
+        imgSrc: 'assets/images/eagle.jpg',
         options: [
           'True',
           'False',
@@ -42,7 +42,7 @@ const questions = [
       {
         id: 5,
         questionText: 'A giraffe has the same number of cervical vertebraes as a human',
-        imgSrc: 'placeholder.png',
+        imgSrc: 'assets/images/giraffe.jpg',
         options: [
           'True',
           'False',
@@ -52,7 +52,7 @@ const questions = [
       {
         id: 6,
         questionText: 'A female house-fly can lay upp to 300 eggs in a lifetime',
-        imgSrc: 'placeholder.png',
+        imgSrc: 'assets/images/houseFly.jpg',
         options: [
           'True',
           'False',
@@ -62,7 +62,7 @@ const questions = [
       {
         id: 7,
         questionText: 'Aside from the elephant and the rhino, the hippopotamus is the biggest land mammal',
-        imgSrc: 'placeholder.png',
+        imgSrc: 'assets/images/hippo.jpg',
         options: [
           'True',
           'False',
@@ -72,7 +72,7 @@ const questions = [
       {
         id: 8,
         questionText: 'A horse of either sex between the ages of 2 and 4 is called a "colt"',
-        imgSrc: 'placeholder.png',
+        imgSrc: 'assets/images/horse.jpg',
         options: [
           'True',
           'False',
@@ -82,7 +82,7 @@ const questions = [
       {
         id: 9,
         questionText: 'The male green anaconda is 2x the size of the female counterpart',
-        imgSrc: 'placeholder.png',
+        imgSrc: 'assets/images/anaconda.jpg',
         options: [
           'True',
           'False',
@@ -92,7 +92,7 @@ const questions = [
       {
         id: 10,
         questionText: 'The den of the otter is called a "sofa"',
-        imgSrc: 'placeholder.png',
+        imgSrc: 'assets/images/otter.jpg',
         options: [
           'True',
           'False',
